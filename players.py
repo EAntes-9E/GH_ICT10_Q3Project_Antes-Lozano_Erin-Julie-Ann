8d066cbc39fcda10d18e9082a7fed35de9ad19e3
EAntes-9E <antes.enr@obmontessori.edu.ph>
2026-02-26 07:29:29 +0800

    Refactor players list and display logic
    Updated the players list format and modified the output display logic in the show_players function.

--- players.py
+++ players.py
@@ -1,28 +1,32 @@
 from pyscript import document
 
 players = [
-    "1) Escudero",
-    "2) Estrada",
-    "3) Tolentino",
-    "4) Pimentel",
-    "5) Binay",
-    "6) Cayetano",
-    "7) Dela Rosa",
-    "8) Ejercito",
-    "9) Gatchalian",
-    "10) Go",
-    "11) Hontiveros",
-    "12) Ko",
-    "13) Legarda",
-    "14) Marcos",
-    "15) Padilla",
-    "16) Poe",
-    "17) Revilla",
-    "18) Tulfo",
-    "19) Villanueva",
-    "20) Villar"
+    "Escudero",
+    "Estrada",
+    "Tolentino",
+    "Pimentel",
+    "Binay",
+    "Cayetano",
+    "Dela Rosa",
+    "Ejercito",
+    "Gatchalian",
+    "Go",
+    "Hontiveros",
+    "Ko",
+    "Legarda",
+    "Marcos",
+    "Padilla",
+    "Poe",
+    "Revilla",
+    "Tulfo",
+    "Villanueva",
+    "Villar"
 ]
 
 def show_players(event):
     output = document.getElementById("player-list")
-    output.innerHTML = "<br>".join(players)
+    
+    output.innerHTML = ""   # clear first
+    
+    for i in range(len(players)):
+        output.innerHTML += f"{i+1}) {players[i]}<br>"
